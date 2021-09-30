7814732ce2aaf2ca214287ac182da43fd9cfd112
Maxwell Zhang <Yiran.zhang@ontariotechu.net>
2021-09-30 14:53:53 -0400

    Initial commit

---
+++ Assets/PLAYER/PlayerCharacterController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayers;
+    [SerializeField] private float runSpeed = 5.5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private Transform[] groundChecks;
+    [SerializeField] private Transform[] wallChecks;
+    [SerializeField] private AudioClip jumpSoundEffect;
+
+
+    private float gravity = -50f;
+    private CharacterController characterController;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float horizontalInput;
+    private bool jumpPressed;
+    private float jumpTimer;
+    private float jumpGracePeriod = 0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = 1;
+
+        //face forward
+        transform.forward = new Vector3(horizontalInput, 0, Mathf.Abs(horizontalInput) - 1);
+
+
+        // isground 
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        else
+        {
+
+            //add gravityy
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        var blocked = false;
+        foreach(var wallCheck in wallChecks)
+        {
+            if(Physics.CheckSphere(wallCheck.position, 0.01f, groundLayers, QueryTriggerInteraction.Ignore))
+            {
+                blocked = true;
+                break;
+            }
+        }
+
+        if (!blocked)
+        {
+            characterController.Move(new Vector3(horizontalInput * runSpeed, 0, 0) * Time.deltaTime);
+        }
+
+        //jump 
+        jumpPressed = Input.GetButtonDown("Jump");
+        if (jumpPressed)
+        {
+            jumpTimer = Time.time;
+        }
+
+        if (isGrounded && (jumpPressed || (jumpTimer > 0 && Time.time < jumpTimer + jumpGracePeriod)))
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+            if(jumpSoundEffect != null)
+            {
+                AudioSource.PlayClipAtPoint(jumpSoundEffect, transform.position, 0.5f);
+            }
+            jumpTimer = -1;
+        }
+
+
+
+
+
+        //vertical velocity
+        characterController.Move(velocity *Time.deltaTime);
+    }
+}
